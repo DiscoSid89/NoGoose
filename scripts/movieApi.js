eb948a570f20b000a9da85d97632f8f02d8cdf00
Raymond Chiu <raymondchiu10@gmail.com>
2025-02-19 16:40:28 -0800

    add api class

---
+++ scripts/movieApi.js
@@ -0,0 +1,63 @@
+// A template for something you are making
+export class MovieApi {
+	constructor() {
+		this.baseUrl = "https://api.themoviedb.org/3";
+		this.bearerToken =
+			"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZTM4NzJhYzY2OWYwODkyZDliMWJkYmQ4YjUyM2RmOCIsIm5iZiI6MTc0MDAwMzY3OS41MDcsInN1YiI6IjY3YjY1OTVmMGRkMjJlYzc5YzNhZWJhNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oucEnlupCiTF29zDYKb8GugSrnaFZ-X5q6jI0Cq2e6Q";
+		this.options = {
+			headers: {
+				accept: "application/json",
+				Authorization: `Bearer ${this.bearerToken}`,
+			},
+		};
+	}
+
+	async testAuthentication() {
+		try {
+			const res = await axios.get(`${this.baseUrl}/authentication`, this.options);
+			return res;
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	async discoverMovies() {
+		const params = {
+			include_adult: "false",
+			include_video: "false",
+			language: "en-US",
+			page: "1",
+			sort_by: "popularity.desc",
+		};
+
+		try {
+			const res = await axios.get(`${this.baseUrl}/discover/movie`, { ...this.options, params });
+			return res;
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	async findMovieByName(query, page = 1) {
+		const params = {
+			query,
+			include_adult: "false",
+			language: "en-US",
+			page,
+			sort_by: "popularity.desc",
+		};
+
+		try {
+			const res = await axios.get(`${this.baseUrl}/search/movie`, { ...this.options, params });
+			console.log(res);
+			testing.textContent = JSON.stringify(res.data.results);
+			return res;
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	async getMoviePoster(imgUrl) {
+		return `https://image.tmdb.org/t/p/original${imgUrl}`;
+	}
+}
