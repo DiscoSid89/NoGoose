de863496a497b0a1eddfd162f2e3aaf5b3ea36bf
Farsid Haque <redbaron_575@hotmail.com>
2025-02-20 10:30:01 -0500

    add api retrieve logic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,10 +11,23 @@ const chosenMovie = movieList.data.results[randomNumber];
 
 console.log("chosenMovie", chosenMovie);
 
+
+
+//answer
+const quizAnswer = document.querySelector(".quiz__answer");
+quizAnswer.textContent =`${chosenMovie.title}`;
+
 console.log("title", chosenMovie.title);
+
+
+//Synopsis
+const synopsis = document.querySelector(".synopsis-gen");
+synopsis.textContent = `"${chosenMovie.overview}"`;
 
 console.log("overview", chosenMovie.overview);
 
+
+//backdrop image
 const moviePoster = api.getMoviePoster(chosenMovie.backdrop_path);
 
 const image = document.querySelector(".hint__background-image");
@@ -32,8 +45,16 @@ const actualMovieGenre = api.parseGenre(genreList, movieGenre[0]);
 
 console.log("actualMovieGenre", actualMovieGenre);
 
-// // ----------------------------- //
+const realMovieGenre = document.querySelector(".hint__genre");
+realMovieGenre.textContent = `${actualMovieGenre}`;
+
+
+// // ------------year----------------- //
 
 const movieReleaseDate = chosenMovie.release_date;
 
 console.log("movieReleaseDate", movieReleaseDate);
+
+const yearHint = document.querySelector(".hint__year");
+
+yearHint.textContent =`${movieReleaseDate}`;
